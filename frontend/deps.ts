9fef2023025bfdf89b3c285d9cd732d2a85d7729
maulepilot117 <chris.white@rubrik.com>
2025-07-27 17:42:03 -0400

    frontend docker config

---
+++ frontend/deps.ts
@@ -0,0 +1,4 @@
+// Frontend dependencies
+export { React } from "https://esm.sh/react@18.2.0";
+export { ReactDOM } from "https://esm.sh/react-dom@18.2.0";
+export { BrowserRouter, Routes, Route, Link } from "https://esm.sh/react-router-dom@6.8.1";
